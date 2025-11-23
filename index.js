96201e335a9c1b047ccf8e4c335fb57bc6b37c55
mdratul4505 <dev.raul.coder@gmail.com>
2025-11-24 01:08:11 +0600

    get all course api

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
 const express = require('express')
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
+// Middleware for body parsing
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mj89i6p.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('education_db');
+    const skillCollection = db.collection('skills')
+
+    // GET all skills
+    app.get('/skills', async (req, res) => {
+      try {
+        const result = await skillCollection.find({}).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Failed to fetch skills" });
+      }
+    });
+
+
+
+
+
+
+    // POST new skill
+    app.post('/skills', async (req, res) => {
+      try {
+        const newSkill = req.body;
+        const result = await skillCollection.insertOne(newSkill);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Failed to add skill" });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected to MongoDB successfully!");
+  } finally {}
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send('Hello i am md ratul hasan!')
+  res.send('Hello world!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
